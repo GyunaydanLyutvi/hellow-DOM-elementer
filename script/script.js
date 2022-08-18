0400cb6b702f1c56eabb54be39bb845a92c660d0
GyunaydanLyutvi <94029221+GyunaydanLyutvi@users.noreply.github.com>
2022-08-18 09:15:46 +0200

    Hello World Aflevering

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const myApp = document.getElementById("app");
+const newDiv = document.createElement("div");
+
+console.log(myApp);
+console.log(newDiv);
+
+newDiv.innerHTML="<H1>Hellow World</H1>";
+
+
+
+myApp. appendChild(newDiv)
