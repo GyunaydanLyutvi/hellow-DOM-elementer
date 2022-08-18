313c0efffbca1c03fce01c77680d208636ff903e
GyunaydanLyutvi <94029221+GyunaydanLyutvi@users.noreply.github.com>
2022-08-18 10:32:15 +0200

    Katte Card

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
 const myApp = document.getElementById("app");
 const newDiv = document.createElement("div");
+newDiv.innerHTML="<H1>Hellow World</H1>"
 
 console.log(myApp);
 console.log(newDiv);
 
-newDiv.innerHTML="<H1>Hellow World</H1>";
 
+const containerr = document.createElement("figure")
+const titel = document.createElement("h2")
+titel.innerText = "CAT VS Brush"
+const image = document.createElement("img")
+image.src = "IMG_4115.jpg"
+image.setAttribute("width",300)
+image.setAttribute("height",500)
+const text = document.createElement("p")
+text.innerText = "Cat against The brush"
 
+containerr. appendChild(titel)
+containerr. appendChild(image)
+containerr. appendChild(text)
+myApp.appendChild(containerr)
 
-myApp. appendChild(newDiv)
